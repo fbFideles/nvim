b2b736503ffd16dedd9d00287fe3d8dde600732d
felipebreno106@gmail.com <felipebreno106@gmail.com>
2022-07-30 22:16:56 -0300

    Adding initial lsp configurations

--- init.lua
+++ init.lua
@@ -1,6 +1,8 @@
 require("user/options")
 require("user/keymaps")
 require("user/plugins")
+require("user/lsp_install")
+require("user/lsp")
 
 vim.cmd "set background=dark"
 vim.cmd "colorscheme gruvbox"
