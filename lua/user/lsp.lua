c4bd6710b498d8bce92d69df04b78c627f1f4b24
felipebreno106@gmail.com <felipebreno106@gmail.com>
2022-08-02 22:54:40 -0300

    Adicionando lazy_load de snips restantes e removendo logs

--- lua/user/lsp.lua
+++ lua/user/lsp.lua
@@ -1,9 +1,5 @@
-print(completionCapabilities)
-
 local lsp_server_list = {"sumneko_lua", "gopls"}
 for indice in ipairs(lsp_server_list) do
-	print(lsp_server_list[indice])
-
 	require("lspconfig")[lsp_server_list[indice]].setup {
 		capabilities = completionCapabilities
 	}
