6cbf04411ab95f37a842c933be5f2afc62b6bf1f
fbFideles <felipebreno106@gmail.com>
2022-12-29 21:10:08 -0300

    Correção de buffer clipboard e instalação de lsps e configuração de lsps automatica

--- lua/user/lsp.lua
+++ lua/user/lsp.lua
@@ -1,6 +1,9 @@
-local lsp_server_list = {"sumneko_lua", "gopls", "tsserver", "eslint", "jedi_language_server", "ccls"}
-for indice in ipairs(lsp_server_list) do
-	require("lspconfig")[lsp_server_list[indice]].setup {
+local lsp_server_list_from_installed = require('nvim-lsp-installer.servers').get_installed_server_names()
+
+local lsp_config = require('lspconfig')
+for i in ipairs(lsp_server_list_from_installed) do
+	local server = lsp_server_list_from_installed[i]
+	lsp_config[server].setup {
 		capabilities = completionCapabilities
 	}
 end
